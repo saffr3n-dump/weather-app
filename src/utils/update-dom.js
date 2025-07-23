2d998dae9a835ec3776ada73ff5e31211968c55c
saffr3n <saffr3n@gmail.com>
2025-07-23 19:52:59 +0300

    feat: add updateDom util

---
+++ src/utils/update-dom.js
@@ -0,0 +1,22 @@
+import getWeatherData from './get-weather-data';
+
+const scaleEls = document.querySelectorAll('.scale');
+const addressEl = document.querySelector('.address');
+const conditionEl = document.querySelector('.condition');
+const realTempValueEl = document.querySelector('.real .value');
+const feelTempValueEl = document.querySelector('.feel .value');
+
+export default async function updateDom(location, scale) {
+  const data = await getWeatherData(location, scale);
+  const { default: iconSrc } = await import(
+    `../assets/weather-icons/${data.icon}`
+  );
+  const icon = Object.assign(new Image(), { src: iconSrc });
+
+  scaleEls.forEach((el) => (el.textContent = scale));
+  addressEl.textContent = data.address;
+  addressEl.after(icon);
+  conditionEl.textContent = data.condition;
+  realTempValueEl.textContent = data.realTemp;
+  feelTempValueEl.textContent = data.feelTemp;
+}
