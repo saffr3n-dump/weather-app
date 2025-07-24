dfd58808e2caebfe3782ece75e15ca7efa187642
saffr3n <saffr3n@gmail.com>
2025-07-24 17:41:37 +0300

    feat: add location search functionality

--- src/index.js
+++ src/index.js
@@ -4,4 +4,12 @@ import updateDom from './utils/update-dom';
 let location = 'Tokyo';
 let scale = 'C';
 
+const searchForm = document.querySelector('.search');
+const searchInput = searchForm.querySelector('#search');
+searchForm.onsubmit = function (e) {
+  e.preventDefault();
+  location = searchInput.value;
+  updateDom(location, scale);
+};
+
 updateDom(location, scale);
